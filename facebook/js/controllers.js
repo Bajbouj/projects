2d78d700193e3d2634806ae67554a7699b13f980
Med <Med>
2016-04-13 17:15:26 +0100

    Création et affichage des status dynamiquement

--- facebook/js/controllers.js
+++ facebook/js/controllers.js
@@ -6,6 +6,7 @@ angular.module("myApp.controllers",[])
     if ($scope.login=="mohamed.jridi@esprit.tn" && $scope.pwd=="med") {
       alert("hello "+$scope.login);
       $state.go("home");
+
     }
     else {
       alert("bye bye");
@@ -18,6 +19,20 @@ angular.module("myApp.controllers",[])
 $scope.fullName="Mohamed Jridi";
 $scope.favoris=["News","Messages","Events","Groups","Football","Series"];
 $scope.groups=["Craft academy","ESPRIT","Engineer"];
+$scope.status=[];
+$scope.getStatus=function(){
+  $scope.stat=false;
+  if (  $scope.newStatus) {
+$scope.status.push($scope.newStatus);
+}
+console.log($scope.newStatus);
+
+  $scope.newStatus="";
+  $scope.stat=true;
+
+
+
+}
 })
 .controller("profilCtrl",function(){
 
